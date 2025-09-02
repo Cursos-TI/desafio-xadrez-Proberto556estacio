eb1b6c46ed2c2685de8f166da7216056b9d45d57
Proberto556estacio <202507269518@alunos.estacio.br>
2025-09-02 17:49:08 -0300

    Desafio Novato - Xadrez

--- xadrez.c
+++ xadrez.c
@@ -8,15 +8,7 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    
     
     //Movimento Bispo (Diagonal) - Utilizando Estrutura Repetição While.
 
